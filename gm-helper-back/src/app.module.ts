cac666b0421265e42696bf6315e47de7e1c56838
Valentin LORQUIN <vavalor@gmail.com>
2022-12-04 18:59:44 +0100

    initialisation db

--- gm-helper-back/src/app.module.ts
+++ gm-helper-back/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(
+      {
+        "type": "mysql",
+        "host": process.env.MYSQL_HOST,
+        "port": 3306,
+        "username": process.env.MYSQL_USERNAME,
+        "password": process.env.MYSQL_PASSWORD,
+        "database": process.env.MYSQL_DB_NAME,
+        "entities": [
+          "dist/**/*.entity{.ts,.js}"
+        ],
+        "synchronize": true,
+      }
+    ),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
